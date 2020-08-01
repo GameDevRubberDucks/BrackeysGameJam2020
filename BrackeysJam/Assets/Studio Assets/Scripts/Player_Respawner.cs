b1e4541e4f8bab041bb4b53c4e7105e0e1fc0fbd
Dan <dmaccormick@hotmail.com>
2020-08-01 13:58:34 -0400

    Created the basis for the respawn system
    - Created a respawn zone prefab
    - Set up scripts to detect collision with them and set the new respawn zone
    - Added a respawn function that moves the player to the new respawn zone

---
+++ BrackeysJam/Assets/Studio Assets/Scripts/Player_Respawner.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class Player_Respawner : MonoBehaviour
+{
+    //--- Private Variables ---//
+    private Respawn_Zone m_currentRespawnZone;
+
+
+
+    //--- Unity Methods ---//
+    private void OnTriggerEnter(Collider other)
+    {
+        // When colliding with a new respawn point, we should keep track of it
+        if (other.tag == "Respawn_ZoneTrigger")
+            RegisterNewRespawnPoint(other.gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            this.Respawn();
+    }
+
+
+
+    //--- Methods ---//
+    public void Respawn()
+    {
+        // Ensure we have a respawn point to go to
+        if (m_currentRespawnZone == null)
+        {
+            Debug.LogError("Error: No respawn zone set!");
+            return;
+        }
+
+        // Move to the respawn point's position and orientation
+        this.transform.position = m_currentRespawnZone.m_respawnPoint.position;
+        this.transform.rotation = m_currentRespawnZone.m_respawnPoint.rotation;
+
+        // TODO: Reset the size of the ball
+    }
+
+
+
+    //--- Utility Functions ---//
+    private void RegisterNewRespawnPoint(GameObject _zoneObj)
+    {
+        // Firstly, we should tell the previous respawn point that it is no longer active
+        if (m_currentRespawnZone != null)
+            m_currentRespawnZone.IsActive = false;
+
+        // Store the new zone's script and activate it
+        var zoneScript = _zoneObj.GetComponentInParent<Respawn_Zone>();
+        m_currentRespawnZone = zoneScript;
+        m_currentRespawnZone.IsActive = true;
+    }
+}
---
+++ BrackeysJam/Assets/Studio Assets/Scripts/Respawn_Zone.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Respawn_Zone : MonoBehaviour
+{
+    //--- Public Variables ---//
+    public Transform m_respawnPoint;
+    public GameObject m_activeIndicator;
+
+
+
+    //--- Properties ---//
+    public bool IsActive
+    {
+        set => m_activeIndicator.SetActive(value);
+    }
+}
