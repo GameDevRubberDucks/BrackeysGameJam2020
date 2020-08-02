d261633b7f30aa7fda1b010ed979f7da9bf14762
Kody Wood <kodymrwood14@gmail.com>
2020-08-02 10:17:07 -0400

    [MOD] Camera Movement

--- BrackeysJam/Assets/Studio Assets/Scripts/Player_CameraZoom.cs
+++ BrackeysJam/Assets/Studio Assets/Scripts/Player_CameraZoom.cs
@@ -10,12 +10,24 @@ public class Player_CameraZoom : MonoBehaviour
     public float m_midRigMinRadius;
     public float m_midRigMinHeight;
 
+    public float m_topRigMinRadius;
+    public float m_topRigMinHeight;
+
+    public float m_botRigMinRadius;
+    public float m_botRigMinHeight;
+
 
 
     //--- Private Variables ---//
     private Player_SizeController m_sizeController;
     private float m_midRigStartRadius;
     private float m_midRigStartHeight;
+
+    private float m_topRigStartRadius;
+    private float m_topRigStartHeight;
+
+    private float m_botRigStartRadius;
+    private float m_botRigStartHeight;
 
 
 
@@ -27,6 +39,16 @@ public class Player_CameraZoom : MonoBehaviour
         var midRigOrbit = m_freeLookCam.m_Orbits[1];
         m_midRigStartRadius = midRigOrbit.m_Radius;
         m_midRigStartHeight = midRigOrbit.m_Height;
+
+
+        var topRigOrbit = m_freeLookCam.m_Orbits[2];
+        m_topRigStartHeight = topRigOrbit.m_Height;
+        m_topRigStartRadius = topRigOrbit.m_Radius;
+
+
+        var botRigOrbit = m_freeLookCam.m_Orbits[0];
+        m_botRigStartRadius = botRigOrbit.m_Radius;
+        m_botRigStartHeight = botRigOrbit.m_Height;
 
         // Register for necessary events
         m_sizeController.OnSizeUpdated.AddListener(UpdateZoom);
@@ -44,11 +66,30 @@ public class Player_CameraZoom : MonoBehaviour
             float newMidRigRadius = Mathf.Lerp(m_midRigStartRadius, m_midRigMinRadius, zoomT);
             float newMidRigHeight = Mathf.Lerp(m_midRigStartHeight, m_midRigMinHeight, zoomT);
 
+            float newTopRigRadius = Mathf.Lerp(m_topRigStartRadius, m_topRigMinRadius, zoomT);
+            float newTopRigHeight = Mathf.Lerp(m_topRigStartHeight, m_topRigMinHeight, zoomT);
+
+            float newBotRigRadius = Mathf.Lerp(m_botRigStartRadius, m_botRigMinRadius, zoomT);
+            float newBotRigHeight = Mathf.Lerp(m_botRigStartHeight, m_botRigMinHeight, zoomT);
+
             // Apply the updated orbit values to the camera
-            var updatedOrbit = new CinemachineFreeLook.Orbit();
-            updatedOrbit.m_Height = newMidRigHeight;
-            updatedOrbit.m_Radius = newMidRigRadius;
-            m_freeLookCam.m_Orbits[1] = updatedOrbit;
+            var updatedMidOrbit = new CinemachineFreeLook.Orbit();
+            updatedMidOrbit.m_Height = newMidRigHeight;
+            updatedMidOrbit.m_Radius = newMidRigRadius;
+
+
+            var updatedTopOrbit = new CinemachineFreeLook.Orbit();
+            updatedTopOrbit.m_Height = newTopRigHeight;
+            updatedTopOrbit.m_Radius = newTopRigRadius;
+
+            var updatedBotOrbit = new CinemachineFreeLook.Orbit();
+            updatedBotOrbit.m_Height = newBotRigHeight;
+            updatedBotOrbit.m_Radius = newBotRigRadius;
+
+
+            m_freeLookCam.m_Orbits[1] = updatedMidOrbit;
+            m_freeLookCam.m_Orbits[2] = updatedTopOrbit;
+            m_freeLookCam.m_Orbits[0] = updatedBotOrbit;
         }
     }
 }
