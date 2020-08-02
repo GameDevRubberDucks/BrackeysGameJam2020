05f0ff4a37ebe53ecd2de49bbb65b4c420e5c8ae
Bo <Wen.yu@ontariotechu.net>
2020-08-02 18:47:20 -0400

    [ADD] Key Item interaction
    - created key prefab
    - players collision detection with key
    - item slot visualization
    - UI visualization to show when the key will drop
    - Drop item after certain point of scaling

---
+++ BrackeysJam/Assets/Studio Assets/Scripts/Item_behavior.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class Item_behavior : MonoBehaviour
+{
+    //public vars
+    //the percentage of size before the item drops
+    public float itemDropPoint;
+    public GameObject model;
+    public bool isHolding;
+
+    //private vars
+    private Vector3 currentPos;
+    private Transform playerTF;
+    private Player_SizeController playerSize;
+
+    private Vector3 initPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPos = transform.position;
+        playerTF = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        playerSize = GameObject.FindObjectOfType<Player_SizeController>();
+        isHolding = false;
+
+    }
+    private void Update()
+    {
+        if(playerSize.PercentOfMaxSize <= itemDropPoint && isHolding)
+        {
+            currentPos = playerTF.position;
+            dropItem();
+        }
+    }
+
+    public void hideitem()
+    {
+        gameObject.GetComponent<SphereCollider>().enabled = false;
+        model.SetActive(false);
+    }
+
+    //reset object location and active state
+    public void rewind()
+    {
+        transform.position = initPos;
+        gameObject.GetComponent<SphereCollider>().enabled = true;
+    }
+
+    //drop the item at the players current position
+    public void dropItem()
+    {
+        model.SetActive(true);
+        transform.position = currentPos;
+        isHolding = false;
+    }
+
+    //corrent position setter
+    public void setCurrentPos(Vector3 pos)
+    {
+        currentPos = pos;
+    }
+
+    // current position getter
+    public Vector3 getCurretnPos()
+    {
+        return currentPos;
+    }
+
+
+}
---
+++ BrackeysJam/Assets/Studio Assets/Scripts/Player_ItemInteractions.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_ItemInteractions : MonoBehaviour
+{
+    //is true is holding key item.
+    public bool holdItem;
+    public Image itemSlot;
+
+    //private variables
+    private Item_behavior itemOnHold;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //update item slot cd
+
+        if (itemOnHold != null && !itemOnHold.isHolding)
+        {
+            itemSlot.enabled = false;
+        }
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //check if coliding with item
+        if (other.gameObject.tag == "Item")
+        {
+            //player is now holding a key
+            holdItem = true;
+            itemOnHold = other.gameObject.GetComponent<Item_behavior>();
+
+
+            //disable the item prefab
+            itemOnHold.hideitem();
+
+            itemSlot.enabled = true;
+            itemOnHold.isHolding = true;
+        }
+    }
+
+}
