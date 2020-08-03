106c7c324e33374009858d58c76e69555cdd5723
Kody Wood <kodymrwood14@gmail.com>
2020-08-03 13:15:09 -0400

    [ADD] Buttons Functionality (Not Complete)

---
+++ BrackeysJam/Assets/Studio Assets/Scripts/Button_Controller.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Controller : MonoBehaviour
+{
+
+    //--- Setup Variable ---//
+    
+    //--- Public Variables ---//
+    public enum Type
+    {
+        Small,
+        Medium,
+        Large
+    }
+    public Type buttonType;
+
+    public bool isPressed;
+    public float lerpSpeed = 1.0f;
+
+    //--- Private Variables ---//
+    private Vector3 upPosition;
+    private Vector3 pressedPosition;
+    private float lerp = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonType = Type.Small;
+        upPosition = transform.position;
+        pressedPosition = upPosition + new Vector3(0.0f, -0.5f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPressed && lerp < 1.0f)
+        {
+            lerp += lerpSpeed * Time.deltaTime;
+            transform.position = Vector3.Lerp(upPosition, pressedPosition, lerp);
+        }
+        else if (!isPressed && lerp > 0.0f)
+        {
+            lerp -= lerpSpeed * Time.deltaTime;
+            transform.position = Vector3.Lerp(upPosition, pressedPosition, lerp);
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            //Check player size, if equal to enum then
+            
+            //if(col.gameObject.GetComponent<Player_SizeController>().GetCurrentSizeState() == buttonType)
+            //{
+            //    isPressed = true;
+            //}
+        }
+    }
+
+    private void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            isPressed = false;
+        }
+    }
+
+}
