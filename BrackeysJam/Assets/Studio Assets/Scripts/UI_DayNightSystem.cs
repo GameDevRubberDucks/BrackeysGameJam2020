cafe4871d96ed7de0abeb37921002fb76e334e2a
Dan <dmaccormick@hotmail.com>
2020-08-03 12:40:55 -0400

    Created a separate day / night system with its own UI bar
    - This system is independent of the scaling but triggers a respawn when the day is over
    - This will be the parent system for visualizations such as the lights dimming, shadows moving, and so on

---
+++ BrackeysJam/Assets/Studio Assets/Scripts/UI_DayNightSystem.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_DayNightSystem : MonoBehaviour
+{
+    //--- Public Variables ---//
+    public Slider m_sldDayNight;
+
+
+
+    //--- Unity Methods ---//
+    private void Awake()
+    {
+        // Register for the event
+        FindObjectOfType<Day_Controller>().OnDayTimeUpdated.AddListener(this.OnDayPercentageUpdated);
+    }
+
+
+
+    //--- Methods ---//
+    public void OnDayPercentageUpdated(float _newDayPercent)
+    {
+        // Update the slider to represent the passage of time
+        m_sldDayNight.value = _newDayPercent;
+    }
+}
