941abce7814059d2d42ff130950571e46e22a640
Dan <dmaccormick@hotmail.com>
2020-08-05 15:11:49 -0400

    Connected the electrified trail to the power box objects

--- BrackeysJam/Assets/Studio Assets/Scripts/Player_TrailParticles.cs
+++ BrackeysJam/Assets/Studio Assets/Scripts/Player_TrailParticles.cs
@@ -12,12 +12,11 @@ public class Player_TrailParticles : MonoBehaviour
     public LayerMask m_groundRayMask;
     public float m_groundOffset;
 
-    public bool useElectric;
-
 
 
     //--- Private Variables ---//
     private Player_SizeController m_sizeController;
+    private ParticleSystemRenderer m_particleRend;
 
 
 
@@ -26,6 +25,13 @@ public class Player_TrailParticles : MonoBehaviour
     {
         // Init the private variables
         m_sizeController = GetComponent<Player_SizeController>();
+        m_particleRend = m_particles.GetComponent<ParticleSystemRenderer>();
+
+        // Find all of the power box puzzles in the scene and register for their completion event
+        // This way, we can disable the electrified trail effect whenever we complete a power box puzzle
+        var powerBoxPuzzles = FindObjectsOfType<PowerBox_Communicator>();
+        foreach (var powerBoxPuzzle in powerBoxPuzzles)
+            powerBoxPuzzle.onPowerBoxesConnected.AddListener(this.EndElectrifiedTrail);
     }
 
     private void Update()
@@ -42,14 +48,16 @@ public class Player_TrailParticles : MonoBehaviour
             emitParams.startSize = m_sizeController.GetCurrentRadius();
             m_particles.Emit(emitParams, 1);
         }
+    }
 
-        if (Input.GetKeyDown(KeyCode.L))
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if we collided with a power box
+        if (collision.gameObject.TryGetComponent<PowerBox_Controller>(out var powerBox))
         {
-            var rend = m_particles.GetComponent<ParticleSystemRenderer>();
-
-            useElectric = !useElectric;
-
-            rend.material = (useElectric) ? m_electrifiedMat : m_baseMat;
+            // If the powerbox is not already active, we should trigger the electricity
+            if (!powerBox.m_isActive)
+                m_particleRend.material = m_electrifiedMat;
         }
     }
 
@@ -60,5 +68,14 @@ public class Player_TrailParticles : MonoBehaviour
     {
         // Clear all of the particles currently in the particle system
         m_particles.Clear();
+
+        // Go back to the normal material
+        m_particleRend.material = m_baseMat;
+    }
+
+    public void EndElectrifiedTrail()
+    {
+        // Go back to the basic trail material
+        m_particleRend.material = m_baseMat;
     }
 }
